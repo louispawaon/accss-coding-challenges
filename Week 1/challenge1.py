bf2e18acf3f0229b79fa19d2b3ce945590e5a351
tremor6916 <lpawaon@gmail.com>
2021-07-23 20:31:12 +0800

    challenge 1 done

--- Week 1/challenge1.py
+++ Week 1/challenge1.py
@@ -9,7 +9,7 @@ For example, if N = 5 and k = 2, the order of executions would be [2, 4, 1, 5, 3
 Bonus: Find an O(log N) solution if k = 2.'''
 
 def survivor(prisoners,k):
-    k-=1 #Execution Begins at k=2
+    k-=1 #Execution Begins with k
     deadman=k #The next dead man
     while len(prisoners)>1:
         prisoners.pop(deadman) #Removing the previous deadman from the list
@@ -17,25 +17,7 @@ def survivor(prisoners,k):
         print(prisoners) #Showing the remaining criminals
     return prisoners[0]
 
-def bonus(k):
-
 N=int(input())
 k=int(input())
 prisoners=list(range(1,N+1)) #Listing Prisoners
-if k==2:
-    print('Bonus Challenge Survivor: ',bonus(N))
 print('Survivor: ',survivor(prisoners, k))
-
-'''
-def josephus(n):
-	
-	# Find value of 2 ^ (1 + floor(Log n))
-	# which is a power of 2 whose value
-	# is just above n.
-	p = 1
-	while p <= n:
-		p *= 2
-
-	# Return 2n - 2^(1 + floor(Logn)) + 1
-	return (2 * n) - p + 1
-'''
