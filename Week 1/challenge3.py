9b5ae418d3171d825c110b05b92e056317454045
tremor6916 <lpawaon@gmail.com>
2021-07-24 08:38:43 +0800

    challenge 3 done - will not submit

--- Week 1/challenge3.py
+++ Week 1/challenge3.py
@@ -5,3 +5,16 @@ Don't use eval or a similar built-in parser.
 
 For example, given '-1 + (2 + 3)', you should return 4.
 '''
+#Attempting to follow PEMDAS
+
+import string
+def evaluation(expression):
+    symbols = '^*()/+-' #Storing basic mathematical symbols, executes only the basics of PEMDAS
+    formula = [(x,expression.index(x)) for x in expression if x in string.digits+symbols] #Storing the expression in iteration with the symbols in a list
+    result = eval(''.join(x[0] for x in formula)) #Evalution
+    expression = expression[:formula[0][1]] + str(result) + expression[formula[-1][1]+1:] #Concatenation of answer
+    return expression
+
+expression=input()
+print(evaluation(expression))
+
