cb084cc4d626df02ea7153816fb537bbb9031519
tremor6916 <lpawaon@gmail.com>
2021-08-07 19:04:08 +0800

    challenge 1 done

--- Week 2/challenge1.java
+++ Week 2/challenge1.java
@@ -9,9 +9,17 @@ reverse("Hello World") ➞ "dlroW olleH"
 reverse("The quick brown fox.") ➞ ".xof nworb kciuq ehT"
 
 reverse("Let's go CS Chameleons!") ➞ "!snoelemahC SC og s'teL"*/
-
 public class challenge1{
-    public static void main(String[] args){
 
+    static void reverse(char[] result){
+        for (int x=result.length-1;x>=0;x--){
+            System.out.print(result[x]); //reversing the character array using iteration
+        }
     }
+    public static void main(String[] args){
+        String attempt = "Let's go CS Chameleons!"; //trial string
+        char[] answer = attempt.toCharArray(); //simple conversion of string to character array
+        reverse(answer); // call reverse method     
+    }
+
 }
