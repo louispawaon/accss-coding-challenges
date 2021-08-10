002bae0c47ffc059f34105d992333febd7f9483d
tremor6916 <lpawaon@gmail.com>
2021-08-10 12:12:33 +0800

    challenge 3 done

--- Week 2/challenge3.java
+++ Week 2/challenge3.java
@@ -8,7 +8,6 @@ Two or more consecutive instances of the letter "s" (i.e. must be together ss..)
 Zero instances of the letter "s" by itself.
 For example:
 
-Sshe ssselects ssso to eat that apple.
 isParselTongue("Sshe ssselects to eat that apple. ") ➞ true
 
 isParselTongue("She ssselects to eat that apple. ") ➞ false
@@ -16,12 +15,13 @@ isParselTongue("She ssselects to eat that apple. ") ➞ false
 
 isParselTongue("Beatrice samples lemonade") ➞ false
 // While "samples" has 2 instances of "s", they are not together.*/
+
 import java.util.*;
 public class challenge3 {
     static boolean isParselTongue(String code){
         boolean decision = false;
-        if(code.contains("ss")){
-            decision=true;
+        if(code.contains("ss")){ 
+            decision=true; //if word may include a "ss" pattern, automatically it will return true
         }
         else{
             decision=false;
@@ -29,26 +29,33 @@ public class challenge3 {
         return decision;  
 }
     public static void main(String[] args){
-        String cipher = "Sshe ssselects ssso to eat that apple.".toLowerCase();
-        String[] message = cipher.split(" ");
-        ArrayList<String>decode=new ArrayList<String>();
+        String cipher = "Sshe ssselects to eat that apple. ".toLowerCase(); //lowercased the trial string to make things easier
+        String[] message = cipher.split(" "); //split the string into a string array 
+        ArrayList<String>decode=new ArrayList<String>(); 
         ArrayList<Boolean>verdict = new ArrayList<Boolean>();
-        for (String s:message){
+        Boolean decision = true;
+        for (String s:message){ //iterate through the message[] and locate any word that would start with "s"
             char ch = s.charAt(0);
             if(ch=='s'){
-                decode.add(s);
+                decode.add(s); //add every word from the message[] to the decode arraylist(arraylist to make things easier)
             }
         }
-        System.out.println(decode.toString());
         for(String x:decode){
-            verdict.add(isParselTongue(x));
+            verdict.add(isParselTongue(x)); //calling my boolean method and add every return value to the verdict arraylist
         }
-        
+        if(verdict.size()<2){ //this applies if there is at most 1 element in the verdict arraylist (to satisfy the condition where there is only 1 word that starts with "s")
+            for(Boolean j:verdict){
+                System.out.println(j);
+            }
+            System.exit(0); 
+        }
+        for(Boolean k:verdict){
+            if(!k.equals(verdict.get(0))){ //checks if all boolean values in the arraylist are all equal to each other
+                decision=false;
+            }
+        }
+        System.out.println(decision); //would either print true(default value) or false
     }
 }
 
-//traverse string 
-//find ascii S or s
-// check if there is another s or S at the left or right of the string
-// if yes=true if no=false
-// get the words with s, then scan them if they have repeating 
+//This is my least ingenious way to solve this challenge and I don't even know if I satisfied the challenge
