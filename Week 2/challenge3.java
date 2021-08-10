5d75c00c2a4c47a56d9690db6434e6297fca3c2a
tremor6916 <lpawaon@gmail.com>
2021-08-10 11:39:39 +0800

    challenge 3 not done - will not submit

--- Week 2/challenge3.java
+++ Week 2/challenge3.java
@@ -1,14 +1,54 @@
 /*Challenge 3
 
-    isParselTongue("Sshe ssselects to eat that apple. ") ➞ true
+Hermione has come up with a precise formula for determining whether or not a phrase was ssspoken by a parssseltongue 
+(a reference from the Harry Potter universe; the language of ssserpents and those who can converse with them).
 
-    isParselTongue("She ssselects to eat that apple. ") ➞ false
-    // "She" only contains one "s".
+Each word in a sssentence must contain either:
+Two or more consecutive instances of the letter "s" (i.e. must be together ss..), or...
+Zero instances of the letter "s" by itself.
+For example:
 
-    isParselTongue("Beatrice samples lemonade") ➞ false
-    // While "samples" has 2 instances of "s", they are not together.*/
+Sshe ssselects ssso to eat that apple.
+isParselTongue("Sshe ssselects to eat that apple. ") ➞ true
+
+isParselTongue("She ssselects to eat that apple. ") ➞ false
+// "She" only contains one "s".
+
+isParselTongue("Beatrice samples lemonade") ➞ false
+// While "samples" has 2 instances of "s", they are not together.*/
+import java.util.*;
 public class challenge3 {
+    static boolean isParselTongue(String code){
+        boolean decision = false;
+        if(code.contains("ss")){
+            decision=true;
+        }
+        else{
+            decision=false;
+        }
+        return decision;  
+}
     public static void main(String[] args){
+        String cipher = "Sshe ssselects ssso to eat that apple.".toLowerCase();
+        String[] message = cipher.split(" ");
+        ArrayList<String>decode=new ArrayList<String>();
+        ArrayList<Boolean>verdict = new ArrayList<Boolean>();
+        for (String s:message){
+            char ch = s.charAt(0);
+            if(ch=='s'){
+                decode.add(s);
+            }
+        }
+        System.out.println(decode.toString());
+        for(String x:decode){
+            verdict.add(isParselTongue(x));
+        }
         
     }
 }
+
+//traverse string 
+//find ascii S or s
+// check if there is another s or S at the left or right of the string
+// if yes=true if no=false
+// get the words with s, then scan them if they have repeating 
