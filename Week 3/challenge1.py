85aef1f8b29e91712088a04455a69be4f3187f34
tremor6916 <lpawaon@gmail.com>
2021-08-27 21:34:49 +0800

    init week 3

---
+++ Week 3/challenge1.py
@@ -0,0 +1,17 @@
+'''
+Challenge #1 Higher Number [EASY]
+Write a function that returns true if there exists at least one number that is larger than or equal to n
+
+For example:
+
+existsHigher([5, 3, 15, 22, 4], 10) ➞ true
+
+existsHigher([1, 2, 3, 4, 5], 8) ➞ false
+
+existsHigher([4, 3, 3, 3, 2, 2, 2], 4) ➞ true
+
+existsHigher([], 5) ➞ false
+
+notes:
+Return false for an empty array [].
+'''
---
+++ Week 3/challenge3.py
@@ -0,0 +1,17 @@
+'''
+It's time to send and receive secret messages.
+
+Create two functions that take a string and an array and return a coded or decoded message.
+
+The first letter of the string, or the first element of the array represents the Character Code of that letter. The next elements are the differences between the characters: e.g. A +3 --> C or z -1 --> y.
+
+For example:
+
+encrypt("Hello") ➞ [72, 29, 7, 0, 3]
+// H = 72, the difference between the H and e is 29 (upper- and lowercase).
+// The difference between the two l's is obviously 0.
+
+decrypt([ 72, 33, -73, 84, -12, -3, 13, -13, -68 ]) ➞ "Hi there!"
+
+encrypt("Sunshine") ➞ [83, 34, -7, 5, -11, 1, 5, -9]
+'''
