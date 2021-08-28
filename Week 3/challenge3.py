6cdb742890d880c8c0d0420799157cd0cd561b2c
tremor6916 <lpawaon@gmail.com>
2021-08-28 12:26:40 +0800

    challenge 3 done

--- Week 3/challenge3.py
+++ Week 3/challenge3.py
@@ -18,36 +18,36 @@ encrypt("Sunshine") ➞ [83, 34, -7, 5, -11, 1, 5, -9]
 
 def encrypt(message):
     encryptedArr=[]
-    placeholder=ord(message[0])
-    encryptedArr.append(placeholder)
-    for i in range(0,len(message)-1):
-        placeholder=(ord(message[i+1])-ord(message[i]))
+    placeholder=ord(message[0]) #For the first character of the message 
+    encryptedArr.append(placeholder) #Immediate append to the encryptedArr
+    for i in range(0,len(message)-1): #To exclude placeholder
+        placeholder=(ord(message[i+1])-ord(message[i])) #Finding the difference of the next placeholders
         encryptedArr.append(placeholder)
     print(encryptedArr)
 
 def decrypt(decryptArr):
     decryptedMess = ""
-    firstLetter=chr(decryptArr[0])
-    secondLetterValue=(abs(decryptArr[1])+(decryptArr[0]))
-    secondLetter=chr(secondLetterValue)
+    firstLetter=chr(decryptArr[0]) #For the first character of the decrypted message
+    nextLetterValue=(abs(decryptArr[1])+(decryptArr[0])) #For the second character since the value of [1]+[0] = ASCII value of the second character in the messaage
+    secondLetter=chr(nextLetterValue)
     decryptedMess=firstLetter+secondLetter
 
     for j in range(2,len(decryptArr)):
-        nextLetters=(secondLetterValue+decryptArr[j])
-        decryptedMess+=chr(nextLetters)
-        secondLetterValue=nextLetters
+        nextLetters=(nextLetterValue+decryptArr[j]) #Using the nextLetterValue to be the template for the succeeding characters of the message
+        decryptedMess+=chr(nextLetters) #Appending to string
+        nextLetterValue=nextLetters #Changing the new value
     print(decryptedMess)
 
-decryptArr=[]
+decryptArr=[] 
 choice=input("encrypt or decrypt?: ")
 
 if choice == "encrypt":
     message=input("Enter message to be encrypted: ")
     encrypt(message)
 elif choice == "decrypt":
-    size = int(input("Enter decrypt message size: "))
+    size = int(input("Enter decrypt message size: ")) #Decrypt Array Size
     for x in range (0, size):
         message=int(input())
         decryptArr.append(message)
-    print(decryptArr)
+    print(decryptArr) #Print Decrypt Array for checking purposes
     decrypt(decryptArr)
