5614747b52a53e133a32f88bc240cdf96d19a179
tremor6916 <lpawaon@gmail.com>
2021-09-04 11:32:46 +0800

    challenge 1 done

--- Week 4/challenge1.java
+++ Week 4/challenge1.java
@@ -10,12 +10,12 @@ timeForMilkAndCookies(new Date(2013, 0, 23)) ➞ false
 
 timeForMilkAndCookies(new Date(3000, 11, 24)) ➞ true
 */
-
-import java.time.LocalDate;
+import java.time.*;
+import java.util.*;
 public class challenge1{
    
     public static boolean timeForMilkAndCookies(LocalDate check){
-        LocalDate christmas = LocalDate.of(0,12,24); //Christmas Eve Template
+        LocalDate christmas = LocalDate.of(0,12,24); //Christmas Eve Template where 1 = January and so on
         int monthChrismas = christmas.getMonthValue(); 
         int dayChristmas = christmas.getDayOfMonth();
         int monthGiven = check.getMonthValue();
@@ -25,8 +25,25 @@ public class challenge1{
         }
         return false;
     }
+    public static boolean timeForMilkAndCookiesVer2(Date check){
+        Date christmas =new Date(0,11,24); //Christmas Eve Template in Date Object where 0 = January and so on
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        Calendar cal2 = Calendar.getInstance(TimeZone.getTimeZone("UTC")); //Using Calendar to retrieve the month and the day of the month 
+        cal.setTime(christmas);
+        cal2.setTime(check);
+        int monthChrismas = cal.get(Calendar.MONTH);
+        int dayChristmas = cal.get(Calendar.DAY_OF_MONTH);
+        int monthGiven = cal2.get(Calendar.MONTH);
+        int dayGiven = cal2.get(Calendar.DAY_OF_MONTH);
+        if((monthChrismas-monthGiven==0)&&(dayChristmas-dayGiven==0)){ //If Both Returns Zero, then it is time for milk and cookies
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args){
-        LocalDate date = LocalDate.of(3000, 11, 24); //Using LocalDate since Date is already Depreciated in Java 8
+        LocalDate date = LocalDate.of(3000, 11, 24); //Using LocalDate since Date Object is already Depreciated in Java 8
+        Date test = new Date(3000, 11, 24); //Using the depreciated Date Object to satisfy the instruction
         System.out.println(timeForMilkAndCookies(date));
+        System.out.println(timeForMilkAndCookiesVer2(test)); //Date Objct test 
     }
 }
